09ba7b7dbac2fcb1dcea4cfa8a896615a7cfca8a
Yasir <basriyasir.simsek99@gmail.com>
2023-08-29 14:46:03 +0300

    v5

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -9,7 +9,5 @@ public class Class01 {
         System.out.println(x+y+z);
         //Versiyon 3
         System.out.println("Versiyon 3 için kodlar");
-        //Versiyon 4
-        System.out.println("Versiyon 4");
     }
 }
